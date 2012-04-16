b66dca727ba8f452d0d6a361ebde81012396bd0a
Ameer Ghouse <aghouse1@gmail.com>
2012-04-16 15:51:43 -0500

    add epic4gtouch to the build script

--- build.sh
+++ build.sh
@@ -59,12 +59,13 @@ fi
 
 targets=("$@")
 if [ 0 = "${#targets[@]}" ] ; then
-    targets=(galaxys2 i777 galaxynote)
+    targets=(galaxys2 i777 epic4gtouch galaxynote)
 fi
 
 MODULES_galaxys2=("drivers/samsung/fm_si4709/Si4709_driver.ko" "drivers/scsi/scsi_wait_scan.ko" "drivers/net/wireless/bcmdhd/dhd.ko" "fs/cifs/cifs.ko" "crypto/md4.ko" "net/dns_resolver/dns_resolver.ko")
 MODULES_galaxynote=("drivers/samsung/fm_si4709/Si4709_driver.ko" "drivers/scsi/scsi_wait_scan.ko" "drivers/net/wireless/bcmdhd/dhd.ko")
 MODULES_i777=("drivers/scsi/scsi_wait_scan.ko" "drivers/net/wireless/bcmdhd/dhd.ko" "fs/cifs/cifs.ko"  "crypto/md4.ko" "net/dns_resolver/dns_resolver.ko")
+MODULES_epic4gtouch=("drivers/scsi/scsi_wait_scan.ko" "drivers/net/wireless/bcmdhd/dhd.ko" "fs/cifs/cifs.ko"  "crypto/md4.ko" "net/dns_resolver/dns_resolver.ko")
 
 START=$(date +%s)
 
